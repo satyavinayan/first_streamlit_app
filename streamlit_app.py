3fbae7d325a27f4065c71ef2c9a464d7f26aeaaa
Satya Vinayan <76174564+satyavinayan@users.noreply.github.com>
2023-09-16 13:36:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ streamlit.text ("The fruit load list contains:")
 streamlit.text (my_data_row)
 
 
-my_data_row = my_cur.fetchone () streamlit.header ("The fruit load list contains:")
+my_data_row = my_cur.fetchone () 
+streamlit.header ("The fruit load list contains:")
 streamlit.dataframe (my_data_row)
 
